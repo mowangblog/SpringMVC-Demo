870cf9f47833b9ba233cec83afa1be451127ab37
Xuan Li <mowangblog@qq.com>
2021-10-08 00:18:35 +0800

    update MVC

--- SpringMVC_MVC/src/main/java/top/mowang/controller/RestfulController.java
+++ SpringMVC_MVC/src/main/java/top/mowang/controller/RestfulController.java
@@ -1,5 +1,11 @@
 package top.mowang.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import top.mowang.pojo.User;
+
 /**
  * Spring-MVC-Demo
  *
@@ -7,5 +13,44 @@ package top.mowang.controller;
  * @website : https://mowangblog.top
  * @date : 2021/10/07 23:11
  **/
+@Controller
 public class RestfulController {
+    /*
+     * 使用restful模拟用户的增删改查
+     * /user GET 查询所有用户信息
+     * /user/1 GET 根据用户id查询用户信息
+     * /user post 添加用户信息
+     * /user/1 Delete 删除用户信息
+     * /user put 修改用户信息
+    */
+
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String getAllUser(){
+        System.out.println("查询所有用户信息");
+        return "success";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.POST)
+    public String saveAllUser(User user){
+        System.out.println("保存用户信息"+user);
+        return "success";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public String updateAllUser(User user){
+        System.out.println("修改用户信息"+user);
+        return "success";
+    }
+
+    @RequestMapping(value = "/user/{id}",method = RequestMethod.GET)
+    public String getUserById(@PathVariable String id){
+        System.out.println("根据id"+id+"查询用户信息");
+        return "success";
+    }
+
+    @RequestMapping(value = "/user/{id}",method = RequestMethod.DELETE)
+    public String deleteUserById(@PathVariable String id){
+        System.out.println("根据id"+id+"删除用户信息");
+        return "success";
+    }
 }
---
+++ SpringMVC_MVC/src/main/java/top/mowang/pojo/User.java
@@ -0,0 +1,79 @@
+package top.mowang.pojo;
+
+/**
+ * Spring-MVC-Demo
+ * 实体类
+ *
+ * @author : Xuan Li
+ * @website : https://mowangblog.top
+ * @date : 2021/10/07 23:15
+ **/
+public class User {
+    private Integer id;
+    private String name;
+    private String password;
+    private String email;
+    private Integer age;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public User() {
+    }
+
+    public User(Integer id, String name, String password, String email, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
